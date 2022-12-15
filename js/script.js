dcbb9ff94efeebd4a382613578a248205a5f8e78
danhastencnc <115740161+danhastencnc@users.noreply.github.com>
2022-12-15 14:52:26 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,39 @@
 // create a map of search terms to URLs
 const searchUrls = new Map();
 searchUrls.set("f-620-003 option a", "https://forms.gle/TvC6a1bnP7YfZq8e9");
+searchUrls.set("f-620-003 option b", "https://forms.gle/FcdLjWd2gf1iZBq3A");
+searchUrls.set("f-620-004", "https://forms.gle/8vNaPUW7GdJ3N2St7");
+searchUrls.set("f-720-001", "https://forms.gle/yowfFXFKqoHWnBqU6");
+searchUrls.set("f-720-002", "https://forms.gle/K4xjfkpMNx2vkvdRA");
+searchUrls.set("f-720-003", "https://forms.gle/w89yqE7MJDsanbPq5");
+searchUrls.set("f-720-004", "https://forms.gle/AK1XppBnDhFCkMVs5");
+searchUrls.set("f-720-005", "https://forms.gle/Ln9iG86WgzmKeR7W8");
+searchUrls.set("f-720-006", "https://forms.gle/U3X1rjxHmijjx8nC6");
+searchUrls.set("f-740-001", "https://forms.gle/iaoUyc58eAirBFxU9");
+searchUrls.set("f-750-002", "https://forms.gle/RGsN2BfqcWcNXqFd7");
+searchUrls.set("f-750-003", "https://forms.gle/j68nPpbpnEuGdjcu7");
+searchUrls.set("f-751-001", "https://forms.gle/2n6tstbDUZLXttKZ6");
+searchUrls.set("f-751-002", "https://forms.gle/BAsy8bv5P3p3wL856");
+searchUrls.set("f-751-003", "https://forms.gle/fh5YSWYkoFXid1cDA");
+searchUrls.set("f-751-004", "https://forms.gle/dnXK8TSbzgUN9d4a9");
+searchUrls.set("f-814-001", "");
+searchUrls.set("f-816-002", "https://forms.gle/XeLzcqBe2eVjen6F7");
+searchUrls.set("f-820-003", "https://forms.gle/2No4pVZ9MRpcnkZf7");
+searchUrls.set("f-840-002", "https://forms.gle/Tuhpd7XCDwts78ndA");
+searchUrls.set("f-840-003", "https://forms.gle/ZeWX7PfuoTUNDxad6");
+searchUrls.set("f-840-004", "https://forms.gle/CTVLRinLZuLXwcuq6");
+searchUrls.set("f-840-005", "https://forms.gle/Kw7WhjKTbDDx9wqG7");
+searchUrls.set("f-840-007", "https://forms.gle/dimCCbXbA5RkXUo7A");
+searchUrls.set("f-850-004", "https://forms.gle/MeHLhqAW2h9AAG3y7");
+searchUrls.set("f-850-011", "https://forms.gle/4eGdoUSPzScR2h6q9");
+searchUrls.set("f-850-012", "https://forms.gle/Wf1VBtXSZgs6LYkR6");
+searchUrls.set("f-850-013", "https://forms.gle/4JQV1bX1ppTeCZdV8");
+searchUrls.set("f-850-014", "https://forms.gle/EEW4jXsfb4YXMB6y7");
+searchUrls.set("f-851-001", "https://forms.gle/v5wFVtKQkKL423Pw5");
+searchUrls.set("f-912-002", "https://forms.gle/eSdzbwVW8bcfSxUH8");
+searchUrls.set("f-912-003", "https://forms.gle/iyi5Bcvo3JZVimdf9");
+searchUrls.set("f-912-004", "https://forms.gle/wTWmxsEgWzW9gaTcA");
+searchUrls.set("f-920-005", "https://forms.gle/mpNo9GAtrQysFckh6");
 
 
 
